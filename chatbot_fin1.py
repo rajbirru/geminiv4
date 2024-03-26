3d9d92cc11b21c1c1e4ff7b010f70e6f1ce04188
arvindk1 <arvindsjunk@gmail.com>
2024-03-26 11:48:28 -0400

    Update chatbot_fin1.py

--- chatbot_fin1.py
+++ chatbot_fin1.py
@@ -224,8 +224,8 @@ with tab1:
         
     # Display a random quote from a financial expert at the bottom of Tab1
     # st.markdown("### Wisdom from Financial Experts")
-    quote = show_random_finance_wisdom()
-    st.markdown(quote)    
+    # quote = show_random_finance_wisdom()
+    # st.markdown(quote)    
 
 with tab2:
     sentiment, sentiment_color, market_sentiment = get_market_sentiment(st.session_state.gemini_chat)
